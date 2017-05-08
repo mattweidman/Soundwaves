238e23c3d65341efdf267039533b96e863a003fa
Matthew Weidman <Matthew Weidman>
2017-05-08 01:30:03 -0400

    removed commented code

--- src/soundwave/Main.java
+++ src/soundwave/Main.java
@@ -8,15 +8,6 @@ import javax.sound.sampled.LineUnavailableException;
 public class Main {
 	
 	public static void main(String args[]) throws IOException {
-		/*Sound[] scale = new Sound[8];
-		for (int i=0; i<scale.length; i++)
-			scale[i] = Sound.note((char)(i%7+'A'), 'n', i/7, 0.25);
-		Sound scalePlay = Sound.concatenate(scale); */
-		
-		/* Sound sounds = Sound.concatenate(Sound.note('D', 'b', -1, 0.5),
-				Sound.note('D', 'n', -1, 0.5),
-				Sound.note('D', '#', -1, 0.5)); */
-		
 		Sound sounds = Sound.fromCSV(new File("data/alma.csv"));
 		
 		Thread t = new Thread(() -> {
